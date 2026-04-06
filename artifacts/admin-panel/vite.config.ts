9ea0fda5c686723e1c649129a3c2798e00172691
Alaaaya <128330294+Alaaaya@users.noreply.github.com>
2026-04-06 22:13:28 +0200

    Update vite.config.ts

--- artifacts/admin-panel/vite.config.ts
+++ artifacts/admin-panel/vite.config.ts
@@ -6,14 +6,14 @@ import path from "path";
 const rawPort = process.env.PORT || "5173";
 const port = Number(rawPort);
 
-const basePath = process.env.BASE_PATH || "/";
-
 export default defineConfig({
-  base: basePath,
+  base: "/", // ✅ أهم تعديل
+
   plugins: [
     react(),
     tailwindcss()
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "src"),
@@ -21,15 +21,19 @@ export default defineConfig({
     },
     dedupe: ["react", "react-dom"],
   },
+
   root: path.resolve(import.meta.dirname),
+
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
+
   server: {
     port,
     host: "0.0.0.0",
   },
+
   preview: {
     port,
     host: "0.0.0.0",
